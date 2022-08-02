6698f7c466a14704525d3cf379c6c635b32b2a1f
Caston_Chen <13226403404@163.com>
2022-08-02 23:47:18 +0800

    update

--- src/main/java/com/caston/hot_search/controller/HotSearchController.java
+++ src/main/java/com/caston/hot_search/controller/HotSearchController.java
@@ -1,14 +1,12 @@
 package com.caston.hot_search.controller;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import org.apache.shiro.authz.annotation.Logical;
 import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,23 +32,29 @@ public class HotSearchController {
     private RestTemplate restTemplate;
 
     @GetMapping("/")
-    public Map<String, JSONObject> hotSearch() {
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
-        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
-
-        Object response = restTemplate.exchange("https://tenapi.cn/resou/", HttpMethod.GET,entity,Object.class);
-
-        System.out.println(response);
-        // JSONObject object = restTemplate.getForObject("https://tenapi.cn/resou/", JSONObject.class);
-        HashMap<String, JSONObject> map = new HashMap<>();
-        // String weibo = get("https://tenapi.cn/resou/");
-        // JSONObject zhihu = get("https://tenapi.cn/zhihuresou/");
-        // JSONObject douyin = get("https://tenapi.cn/douyinresou/");
-        // JSONObject blibli = get("https://tenapi.cn/bilihot/");
-        // JSONObject baidu = get("https://tenapi.cn/baiduhot/");
+    public Map<String, JSONArray> hotSearch() {
+        HashMap<String, JSONArray> map = new HashMap<>();
+        JSONArray weibo = get("https://tenapi.cn/resou/");
+        JSONArray zhihu = get("https://tenapi.cn/zhihuresou/");
+        JSONArray douyin = get("https://tenapi.cn/douyinresou/");
+        JSONArray blibli = get("https://tenapi.cn/bilihot/");
+        JSONArray baidu = get("https://tenapi.cn/baiduhot/");
+        map.put("微博",weibo);
+        map.put("知乎",zhihu);
+        map.put("抖音",douyin);
+        map.put("B站",blibli);
+        map.put("百度",baidu);
         return map;
     }
 
+    public JSONArray get(String url){
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36");
+        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+
+        String response = restTemplate.exchange(url, HttpMethod.GET,entity,String.class).getBody();
+        JSONObject jsonObject = JSONObject.parseObject(response);
+        JSONArray jsonArray = jsonObject.getJSONArray("list");
+        return  jsonArray;
+    }
 }
